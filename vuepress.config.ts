73966a67be9ec4972547950d3cb9f52f654ce48b
leoncool23 <125378580+leoncool23@users.noreply.github.com>
2025-01-01 17:44:25 +0800

    Update vuepress.config.ts

--- vuepress.config.ts
+++ vuepress.config.ts
@@ -36,8 +36,8 @@ export default defineUserConfig({
     {
       mountPath: "/huggingface测试",
       analysis: huggingFaceDatasetsAnalysis({
-        userName: "Open-Orca",
-        datasetsName: "OpenOrca",
+        userName: "leonsimon23",
+        datasetsName: "test",
         branchName: "main",
         path: "/",
         //最大深度,如果文件夹有很多层最大递归解析多少层，默认10
